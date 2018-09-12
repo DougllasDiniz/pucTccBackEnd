b84ed378cd488b5f07b05013ac4d90af172945af
Dougllas Diniz <42546102+DougllasDiniz@users.noreply.github.com>
2018-09-12 17:31:49 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -3,23 +3,33 @@
 require 'vendor/autoload.php';
 require 'ViewModel/ViewModelLogin.php';
 
-// 	Cria e configura o Slim
-$config = ['settings' => [    
-    'addContentLengthHeader' => false,
-]];
-$app = new \Slim\App($config);
+class WebServiceLogin{
 
-//rota do post para buscar os dados no banco de dados
-$app->post('/login[/]', function ($request, $response) use ($app) {
-   
-    $dado = $request->getParsedBody();    
-    $con = new ViewModelLogin();
-    $user = $con->dadosUsuario($dado[email],$dado[senha]);
-    
-    return $user;    
-});
+    public function callHttpPost(){
+        // 	Cria e configura o Slim
+        $config = ['settings' => [    
+            'addContentLengthHeader' => false,
+        ]];
+        $app = new \Slim\App($config);
 
-// Run app
-$app->run();
+        //rota do post para buscar os dados no banco de dados
+        $app->post('/login[/]', function ($request, $response) use ($app) {
+        
+            $dado = $request->getParsedBody();    
+            $con = new ViewModelLogin();
+            $user = $con->dadosUsuario($dado[email],$dado[senha]);
+            
+            return $user;
+            //print_r($dado[email]);
+            
+        });
+
+        // Run app
+        $app->run();
+    }
+}
+
+$dadosPost = new WebServiceLogin();
+$dadosPost->callHttpPost();
 
 ?>
